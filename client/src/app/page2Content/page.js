6b338ed9a88499736587f5422e9086411082cffa
Suraj Singhal <117528760+MrStark1234@users.noreply.github.com>
2024-04-18 14:49:16 +0530

    routes changes

--- client/src/app/page2Content/page.js
+++ client/src/app/page2Content/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import RightCont from "../p2ContentRight/page";
+import Link from "next/link";
 
 const Page2Content = () => {
   return (
@@ -15,41 +16,17 @@ const Page2Content = () => {
               You focus on helping your friends and family, we take care of
               everything else.
             </p>
-            <button className="rounded-lg px-4 py-3 mt-10 text-white bg-[#4A508E] text-sm">
-              Get Started
-            </button>
+            <Link href="/">
+              <button className="rounded-lg px-4 py-3 mt-10 text-white bg-[#4A508E] text-sm">
+                Get Started
+              </button>
+            </Link>
           </div>
         </div>
         <div className="right w-[55%] h-[1925px] ">
           <RightCont number="1" />
           <RightCont number="2" />
           <RightCont number="3" />
-
-          {/* <div className="m-[40px] p-[80px]">
-            <div className="flex justify-center items-center gap-[50px]">
-              <div className="w-[80px] h-[45px] rounded-lg flex justify-center items-center bg-[#4a508e]">
-                <h1 className="text-white text-lg font-bold">1</h1>
-              </div>
-              <h1 className="text-2xl font-semibold head">
-                Share the holidays, stays and experiences with your friends and
-                family
-              </h1>
-            </div>
-
-            <p className="font-light mt-4 ml-[100px]">
-              You receive trending & curated Itineraries and Insider deals on
-              Hotels & Flights to share
-            </p>
-            <div className="flex justify-center items-center flex-col">
-              <img src="/assets/image.png" className="mt-6 ml-[100px]" />
-
-              <button className="rounded-lg px-4 py-3 mt-10 text-white bg-[#5740e4] text-sm ml-[100px]">
-                Share
-              </button>
-            </div>
-
-          
-          </div> */}
         </div>
       </div>
     </div>
